60d806f4c209f656ed7d16a5e9db9514fc415cd4
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-05-08 10:55:17 -0300

    Construindo o arquivo responsável pelo DataSource para migrações

--- src/common/config/typeorm_config.migrations.ts
+++ src/common/config/typeorm_config.migrations.ts
@@ -1,1 +1,29 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const dataSourceOptions: DataSourceOptions = {
+  type: process.env.DATABASE_TYPE as any || 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: 5432,
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || '44332211',
+  database: process.env.DATABASE_NAME || 'SPG_NESTJS',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  synchronize: false,
+  migrationsRun: true,
+  logging: true,
+  logger: 'advanced-console',
+};
+
+export const AppDataSource = new DataSource(dataSourceOptions);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('DataSource inicializado com sucesso');
+  })
+  .catch((error) => {
+    console.error('Erro ao inicializar o DataSource', error);
+  });
